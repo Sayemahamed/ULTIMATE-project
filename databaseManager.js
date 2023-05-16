d8ddf98c9c0d88352ed6cff436722e66420509d3
Sayem <sayemahamed183@gmail.com>
2023-05-16 21:08:15 +0600

    ok

--- databaseManager.js
+++ databaseManager.js
@@ -24,9 +24,9 @@ let enterNewRecord = (name, url, price, type, gender) => {
   );
 };
 // enterNewRecord(
-//   "White-Buttoned-Shirt",
-//   "https://images.pexels.com/photos/297933/pexels-photo-297933.jpeg?cs=srgb&dl=pexels-david-bartus-297933.jpg&fm=jpg&w=640&h=427&_gl=1*jddfqm*_ga*OTkwODAwMDcuMTY4MTM3NTExMg..*_ga_8JE65Q40S6*MTY4Mzc0MzM1My43LjEuMTY4Mzc0NjIwOC4wLjAuMA..",
-//   "120",
+//   "Standard-Shirt",
+//   "https://images.pexels.com/photos/10952733/pexels-photo-10952733.jpeg?cs=srgb&dl=pexels-farhad-chaudhary-10952733.jpg&fm=jpg&_gl=1*16fz7ae*_ga*OTkwODAwMDcuMTY4MTM3NTExMg..*_ga_8JE65Q40S6*MTY4NDI0ODY5MC45LjEuMTY4NDI0OTI5OS4wLjAuMA..",
+//   "115",
 //   "mansTops",
 //   "male"
 // );
